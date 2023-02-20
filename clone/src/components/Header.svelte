ba281bf79812dbfca8cf337cacaf849a473efaa6
wkkim <wkkim@tidesquare.com>
2023-02-20 22:12:16 +0900

    a

---
+++ clone/src/components/Header.svelte
@@ -0,0 +1,22 @@
+<header>
+    <img 
+        src="/images/trello-logo.svg"
+        alt="Trello"
+        class="logo" />
+</header>
+
+<style>
+    header {
+        height: 40px;
+        box-sizing: border-box;
+        background-color: rgba(0,0,0,0.4);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header img.logo {
+        width:  80px;
+        opacity: .5;
+    }
+</style>
